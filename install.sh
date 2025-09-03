6585f035222b6c7fc9ba7d8fabbc9024b15fa826
Andrew Hale <andrew@andrewahale.com>
2025-09-03 22:34:17 +1000

    personal

--- install.sh
+++ install.sh
@@ -34,25 +34,12 @@ source $OMARCHY_INSTALL/config/increase-sudo-tries.sh
 source $OMARCHY_INSTALL/config/increase-lockout-limit.sh
 source $OMARCHY_INSTALL/config/ssh-flakiness.sh
 source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-source $OMARCHY_INSTALL/config/xcompose.sh
-source $OMARCHY_INSTALL/config/mise-ruby.sh
 source $OMARCHY_INSTALL/config/docker.sh
 source $OMARCHY_INSTALL/config/mimetypes.sh
 source $OMARCHY_INSTALL/config/localdb.sh
-source $OMARCHY_INSTALL/config/sudoless-asdcontrol.sh
 source $OMARCHY_INSTALL/config/hardware/network.sh
-source $OMARCHY_INSTALL/config/hardware/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/hardware/bluetooth.sh
-source $OMARCHY_INSTALL/config/hardware/printer.sh
 source $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
-source $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 source $OMARCHY_INSTALL/config/hardware/nvidia.sh
-source $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
-
-# Login
-source $OMARCHY_INSTALL/login/plymouth.sh
-source $OMARCHY_INSTALL/login/limine-snapper.sh
-source $OMARCHY_INSTALL/login/alt-bootloaders.sh
 
 # Finishing
 source $OMARCHY_INSTALL/reboot.sh
